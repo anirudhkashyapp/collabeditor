7c64810a33c37b513d62d3774722ede499e7a7aa
Anirudh <anisureshkashyap@gmail.com>
2026-04-13 13:58:03 +0530

    initial setup: spring boot + postgresql connected

---
+++ src/main/java/com/collabeditor/collabeditor/CollabeditorApplication.java
@@ -0,0 +1,13 @@
+package com.collabeditor.collabeditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabeditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabeditorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collabeditor/collabeditor/CollabeditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collabeditor.collabeditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabeditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
